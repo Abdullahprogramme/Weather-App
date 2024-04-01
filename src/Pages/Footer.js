24c5d056868837b94124f6341afed2bb2d2474b4
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-01 20:36:04 +0500

    Update Footer.js

--- src/Pages/Footer.js
+++ src/Pages/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 import { FaLinkedin } from "react-icons/fa";
 
 const Footer = () => (
-    <footer className="border-t-2 border-[#222831] text-center font-bold text-[#7AA2E3] p-2 flex flex-row justify-center items-center bg-[#222831] mx-auto w-screen space-x-20"
-        style={{ fontFamily: 'Pacifico, cursive' }}
+    <footer className="fixed bottom-0 mt-2 border-t-2 border-[#222831] text-center font-bold text-[#7AA2E3] p-0 flex flex-row justify-center items-center bg-[#222831] mx-auto w-screen space-x-20"
+        style={{ fontFamily: 'Pacifico, cursive', zIndex: '1000'}}
     >
         <p>&copy; 2021 Weather App</p>
         <a className="flex flex-row text-[#7AA2E3]" href="https://www.linkedin.com/in/abdullahtariq78/" target="_blank" rel="noopener noreferrer">
