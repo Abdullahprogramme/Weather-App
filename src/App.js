0316f1e6769b4750aa6227720fb5d4a2bf187f1f
Abdullah Tariq <111647623+Abdullahprogramme@users.noreply.github.com>
2024-04-07 21:39:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import ForecastPage from './Pages/ForecastPage';
 import Navbar from './Pages/Navbar';
 import Footer from './Pages/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Background from './Store/Background.jpg';
 
 function App() {
   return (
-    <div>
+    <div style={{
+      backgroundImage: `url(${Background})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    }}>
 
       <Router>
         <Navbar />
